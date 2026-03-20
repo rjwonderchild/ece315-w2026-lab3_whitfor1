ca631e79cffe3cc1cdf639aa41654a2faa5eed4c
R W <whitfor1@ualberta.ca>
2026-03-20 01:37:42 -0600

    Update move.c

--- lab3_part2_student/game/move.c
+++ lab3_part2_student/game/move.c
@@ -1,7 +1,8 @@
 #include <stdbool.h>
+#include <stdio.h>
 #include "object.h"
 #include "misc.h"
-#include "game_io.h"
+#include "game_state.h"
 
 static int weightOfContents(OBJECT *container)
 {
@@ -18,21 +19,30 @@ static void describeMove(OBJECT *obj, OBJECT *to)
 {
    if (to == player->location)
    {
-      GameIO_Printf("You drop %s.\n", obj->description);
+      printf("You drop %s.\n", obj->description);
    }
    else if (to != player)
    {
-      GameIO_Printf(to->health > 0 ? "You give %s to %s.\n" : "You put %s in %s.\n",
-                    obj->description, to->description);
+      printf(to->health > 0 ? "You give %s to %s.\n" : "You put %s in %s.\n",
+             obj->description, to->description);
    }
    else if (obj->location == player->location)
    {
-      GameIO_Printf("You pick up %s.\n", obj->description);
+      printf("You pick up %s.\n", obj->description);
    }
    else
    {
-      GameIO_Printf("You get %s from %s.\n",
-                    obj->description, obj->location->description);
+      printf("You get %s from %s.\n",
+             obj->description, obj->location->description);
+   }
+}
+
+static void handleSpecialPickup(OBJECT *obj, OBJECT *to)
+{
+   if (to == player && obj == thering)
+   {
+      gRgbUnlocked = true;
+      printf("As you take the Ring, a strange power wakes within the dark.\n");
    }
 }
 
@@ -44,25 +54,26 @@ void moveObject(OBJECT *obj, OBJECT *to)
    }
    else if (to == NULL)
    {
-      GameIO_PutString("There is nobody here to give that to.\n");
+      printf("There is nobody here to give that to.\n");
    }
    else if (to->capacity == 0)
    {
-      GameIO_PutString(obj == keyForBox && (to == closedBox || to == lockedBox) ?
-                       "The key seems to fit the lock.\n" :
-                       "It doesn't seem to fit in.\n");
+      printf(obj == keyForBox && (to == closedBox || to == lockedBox) ?
+                "The key seems to fit the lock.\n" :
+                "It doesn't seem to fit in.\n");
    }
    else if (obj->weight > to->capacity)
    {
-      GameIO_PutString("That is way too heavy.\n");
+      printf("That is way too heavy.\n");
    }
    else if (obj->weight + weightOfContents(to) > to->capacity)
    {
-      GameIO_PutString("That would become too heavy.\n");
+      printf("That would become too heavy.\n");
    }
    else
    {
       describeMove(obj, to);
       obj->location = to;
+      handleSpecialPickup(obj, to);
    }
 }
