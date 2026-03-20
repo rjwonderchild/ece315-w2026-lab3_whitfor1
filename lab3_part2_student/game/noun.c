0a808a743070de5925e7d6dea0f9b18d2377bc22
Riley Whitford <whitfor1@ualberta.ca>
2026-03-19 22:52:08 -0600

    Added directional functionality.

--- lab3_part2_student/game/noun.c
+++ lab3_part2_student/game/noun.c
@@ -5,8 +5,17 @@
 #include "misc.h"
 static bool objectHasTag(OBJECT *obj, const char *noun)
 {
-   return noun != NULL && *noun != '\0' && strcmp(noun, obj->tag) == 0;
+   if (noun != NULL && *noun != '\0')
+   {
+      const char **tag;
+      for (tag = obj->tags; *tag != NULL; tag++)
+      {
+         if (strcmp(*tag, noun) == 0) return true;
+      }
+   }
+   return false;
 }
+static OBJECT ambiguousNoun;
 static OBJECT *getObject(const char *noun, OBJECT *from, DISTANCE maxDistance)
 {
    OBJECT *obj, *res = NULL;
@@ -14,7 +23,7 @@ static OBJECT *getObject(const char *noun, OBJECT *from, DISTANCE maxDistance)
    {
       if (objectHasTag(obj, noun) && getDistance(from, obj) <= maxDistance)
       {
-         res = obj;
+         res = res == NULL ? obj : &ambiguousNoun;
       }
    }
    return res;
@@ -32,6 +41,11 @@ OBJECT *getVisible(const char *intention, const char *noun)
       {
          printf("You don't see any %s here.\n", noun);
       }
+   }
+   else if (obj == &ambiguousNoun)
+   {
+      printf("Please be specific about which %s you mean.\n", noun);
+      obj = NULL;
    }
    return obj;
 }
@@ -57,6 +71,12 @@ OBJECT *getPossession(OBJECT *from, const char *verb, const char *noun)
          printf("There appears to be no %s you can get from %s.\n",
                 noun, from->description);
       }
+   }
+   else if (obj == &ambiguousNoun)
+   {
+      printf("Please be specific about which %s you want to %s.\n",
+             noun, verb);
+      obj = NULL;
    }
    else if (obj == from)
    {
--- lab3_part2_student/game/object.h
+++ lab3_part2_student/game/object.h
@@ -1,12 +1,10 @@
 typedef struct object {
    const char    *description;
-   const char    *tag;
+   const char   **tags;
    struct object *location;
    struct object *destination;
 } OBJECT;
-
 extern OBJECT objs[];
-
 #define field      (objs + 0)
 #define cave       (objs + 1)
 #define silver     (objs + 2)
@@ -15,5 +13,6 @@ extern OBJECT objs[];
 #define player     (objs + 5)
 #define intoCave   (objs + 6)
 #define exitCave   (objs + 7)
-
-#define endOfObjs  (objs + 8)
+#define wallField  (objs + 8)
+#define wallCave   (objs + 9)
+#define endOfObjs  (objs + 10)
