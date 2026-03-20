634ed540222aca4ae940fa4aa745a6a5d434a9e9
R W <whitfor1@ualberta.ca>
2026-03-20 01:44:53 -0600

    Update game_state.c

--- lab3_part2_student/game/game_state.c
+++ lab3_part2_student/game/game_state.c
@@ -1,4 +1,26 @@
 #include "game_state.h"
 
-bool gRgbUnlocked = false;
+bool gHasRing = false;
+bool gHasSting = false;
+
+bool gRingDrawn = false;
+bool gStingDrawn = false;
+
 bool gRiddleCompleted = false;
+RGBMode gRgbMode = RGB_MODE_OFF;
+
+void GameState_UpdateRgbMode(void)
+{
+    if (gRingDrawn)
+    {
+        gRgbMode = RGB_MODE_YELLOW;
+    }
+    else if (gStingDrawn)
+    {
+        gRgbMode = RGB_MODE_CYAN;
+    }
+    else
+    {
+        gRgbMode = RGB_MODE_OFF;
+    }
+}
